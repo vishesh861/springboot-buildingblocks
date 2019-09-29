ea10b5e25e07b65b671602f6c90a65c6ee56d468
vishesh861 <vishesh861994@gmail.com>
2019-09-29 17:46:50 +0530

    JPA Rest Services Development Completed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(101,'vishesh861994@gmail.com','vishesh','agarwal','dev','ssn101','admin');
+insert into user values(102,'mohit@gmail.com','mohit','agarwal','dev','ssn102','admin1');
+insert into user values(103,'amit861994@gmail.com','amit','agarwal','dev','ssn103','admin2');
