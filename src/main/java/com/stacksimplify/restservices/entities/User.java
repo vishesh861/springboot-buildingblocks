3dd83eb8e072f1a2e66f6d68a63a3c2020e62232
vishesh861 <vishesh861994@gmail.com>
2019-10-22 00:26:37 +0530

    Json Ignore First Commit

--- src/main/java/com/stacksimplify/restservices/entities/User.java
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -13,9 +13,12 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import org.springframework.hateoas.ResourceSupport;
 @Entity
 @Table(name = "user")
+@JsonIgnoreProperties({"firstname","lastname"})
 public class User extends ResourceSupport {
 	
 		
@@ -41,6 +44,7 @@ public class User extends ResourceSupport {
 	private String role;
 	
 	@Column(name="SSN",length=50,nullable=false,unique=true)
+	@JsonIgnore
     private String ssn;
 	
 	@OneToMany(mappedBy = "user")
